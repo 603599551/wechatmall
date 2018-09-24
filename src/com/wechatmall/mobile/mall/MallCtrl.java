1e8316229b182af0aa2ecf0dc5de996f37e9c54d
lihongxuan <qq807572809@live.com>
2018-09-24 09:24:36 +0800

    实现商品详细信息查询和商品列表点击加入购物车

--- src/com/wechatmall/mobile/mall/MallCtrl.java
+++ src/com/wechatmall/mobile/mall/MallCtrl.java
@@ -120,7 +120,7 @@ public class MallCtrl extends BaseCtrl {
         "originalPrice": 20.0,
         "presentPrice": 18.0,
         "name": "过水手擀面",
-        "describe": "描述详情",
+        "detail": "描述详情",
         "types": ["芥末味", "番茄味"]
         }
     }
@@ -137,7 +137,51 @@ public class MallCtrl extends BaseCtrl {
      * }
      */
     public void showCommodityDetails(){
-        renderJson("{\"code\":1,\"goodsList\":{\"url\":\"http://pic34.photophoto.cn/20150113/0006019095934688_b.jpg\",\"desTit\":\"商品的描述\",\"originalPrice\":20,\"presentPrice\":18,\"name\":\"过水手擀面\",\"describe\":\"描述详情\",\"types\":[\"芥末味\",\"番茄味\"]}}");
+        JsonHashMap jhm=new JsonHashMap();
+        /**
+         * 接收前端参数
+         */
+
+        //客户id
+        String userId  = getPara("userId");
+
+        //商品id
+        String goodsId = getPara("goodsId");
+
+        //用户id非空验证
+        if (userId == null||userId.length() <= 0){
+            jhm.putCode(0).putMessage("客户id为空！");
+            renderJson(jhm);
+            return;
+        }
+
+        //商品id非空验证
+        if(goodsId == null||goodsId.length() <= 0) {
+            jhm.putCode(0).putMessage("商品id为空！");
+            renderJson(jhm);
+            return;
+        }
+
+        try {
+            /*
+             *查询商品详细信息
+             *查询w_product表中pname,pcost,price,picture,pintroduction,pdetail,pkeyword字段。
+             */
+            String sql = "select pname name,pcost originalPrice,price presentPrice,picture url,pintroduction desTit,pdetail detail,pkeyword types from w_product where pid = ?";
+            Record viewGoods = Db.findFirst(sql,goodsId);
+            if (viewGoods != null){
+                String []types=viewGoods.getStr("types").split(",");
+                viewGoods.set("types",types);
+                jhm.put("goodsList",viewGoods);
+            } else {
+                jhm.putCode(0).putMessage("查询失败!");
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+            jhm.putCode(-1).putMessage("服务器发生异常！");
+        }
+        renderJson(jhm);
+        //renderJson("{\"code\":1,\"goodsList\":{\"url\":\"http://pic34.photophoto.cn/20150113/0006019095934688_b.jpg\",\"desTit\":\"商品的描述\",\"originalPrice\":20,\"presentPrice\":18,\"name\":\"过水手擀面\",\"describe\":\"描述详情\",\"types\":[\"芥末味\",\"番茄味\"]}}");
     }
 
     /**
@@ -176,7 +220,58 @@ public class MallCtrl extends BaseCtrl {
      * }
      */
     public void modifyShoppingCart(){
-        renderJson("{\"code\":1,\"message\":\"提交成功！\"}");
+        JsonHashMap jhm=new JsonHashMap();
+        /**
+         * 接收前端参数
+         */
+
+        //客户id
+        String userId  = getPara("userId");
+
+        //商品id
+        String goodsId = getPara("goodsId");
+
+        //商品数量
+        String goodsNum = getPara("goodsNum");
+
+        //用户id非空验证
+        if (userId == null||userId.length() <= 0){
+            jhm.putCode(0).putMessage("客户id为空！");
+            renderJson(jhm);
+            return;
+        }
+
+        //商品id非空验证
+        if(goodsId == null||goodsId.length() <= 0) {
+            jhm.putCode(0).putMessage("商品id为空！");
+            renderJson(jhm);
+            return;
+        }
+
+        //商品数量非空验证
+        if(goodsNum == null || goodsNum.length() <=0){
+            jhm.putCode(0).putMessage("商品数量为空！");
+            renderJson(jhm);
+            return;
+        }
+
+        try{
+            /**
+             *商品列表点击加入购物车
+             */
+            String modifyTime = DateTool.GetDateTime();
+            int flag = Db.update("update w_shoppingcart set scquantity = ?, scmodifier = ? ,scmodify_time = ? where pid=? and cid=?",goodsNum,userId,modifyTime,goodsId,userId);
+            if(flag>0){
+                jhm.putMessage("修改成功！");
+            }else {
+                jhm.putCode(0).putMessage("修改失败！");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            jhm.putCode(-1).putMessage("服务器发生异常！");
+        }
+        renderJson(jhm);
+        //renderJson("{\"code\":1,\"message\":\"提交成功！\"}");
     }
 
     /**
