a6fd404114ff3f8bef0d7cb8546ab1ae4a448408
zhanjinqi <603599551@qq.com>
2018-10-01 14:55:25 +0800

    订单打印功能- 待测试

--- src/com/wechatmall/pc/order/PrintCtrl.java
+++ src/com/wechatmall/pc/order/PrintCtrl.java
@@ -12,6 +12,8 @@ import com.utils.UserSessionUtil;
 import easy.util.DateTool;
 import easy.util.NumberUtils;
 import easy.util.UUIDTool;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 import org.apache.commons.lang.StringUtils;
 import utils.bean.JsonHashMap;
 
@@ -23,97 +25,109 @@ import java.util.Map;
 
 public class PrintCtrl extends BaseCtrl {
 
-    //private static final String[] outgoing_goods_one_page_arr = {"warehouse_name","date","order_num","store_name"};
     private static final String[] ORDER_GOODS_ONE_PAGE_ARR = {"order_num","buy_time","receiver_name","phone","address","pay_status","transport_type","pay_type"};
 
     public void printSingleOrder()throws UnsupportedEncodingException{
         JsonHashMap jhm = new JsonHashMap();
-        String orderId = getPara("id");
-        if (StringUtils.isEmpty(orderId)){
+        String idList = getPara("idList");
+        if (StringUtils.isEmpty(idList)){
             jhm.putCode(0).putMessage("订单id为空");
             renderJson(jhm);
             return;
         }
-        Record dataRecord = Db.findFirst("SELECT o.oid AS order_num,o.ocreate_time AS buy_time,o.oname AS receiver_name,o.ophone AS phone,o.oaddress AS address,d.name AS pay_status,dd.name AS transport_type ,ddd.name AS pay_type FROM w_orderform o LEFT JOIN w_dictionary d ON d.value=o.ostatus LEFT JOIN w_dictionary dd ON dd.value=o.otransport_type LEFT JOIN w_dictionary ddd ON ddd.value=o.opay_type WHERE o.oid=?", orderId);
-        if(dataRecord == null){
-            jhm.putCode(-1).putMessage("订单号有错误，请确认订单！");
-            renderJson(jhm);
-            return;
-        }
-        Map<String, String> onePageData = new HashMap<>();
-        for(String s : ORDER_GOODS_ONE_PAGE_ARR){
-            onePageData.put(s, dataRecord.getStr(s));
-        }
-        File onePageTemp = new File(this.getRequest().getSession().getServletContext().getRealPath("") + "/template/order.template");
-        BufferedReader br = null;
-        String onePageTempStr = "";
-        try {
-            br = new BufferedReader(new InputStreamReader(new FileInputStream(onePageTemp), "utf-8"));
-            String str = br.readLine();
-            while(str != null){
-                onePageTempStr += str;
-                str = br.readLine();
+        JSONArray orderIdList=JSONArray.fromObject(idList);
+        int idLen=orderIdList.size();
+        String content="";
+        PDFUtil pdfUtil = new PDFUtil(this.getRequest(), "template");
+
+        //遍历订单id
+        for (int i=0;i<idLen;i++){
+            String orderId=orderIdList.getJSONObject(i).getString("id");
+            Record dataRecord = Db.findFirst("SELECT o.oid AS order_num,o.ocreate_time AS buy_time,o.oname AS receiver_name,o.ophone AS phone,o.oaddress AS address,d.name AS pay_status,dd.name AS transport_type ,ddd.name AS pay_type,o.ooriginal_sum AS product_sum,o.ocurrent_sum AS should_pay FROM w_orderform o LEFT JOIN w_dictionary d ON d.value=o.ostatus LEFT JOIN w_dictionary dd ON dd.value=o.otransport_type LEFT JOIN w_dictionary ddd ON ddd.value=o.opay_type WHERE o.oid=?", orderId);
+            if(dataRecord == null){
+                jhm.putCode(-1).putMessage("订单号有错误，请确认订单！");
+                renderJson(jhm);
+                return;
             }
+            Map<String, String> onePageData = new HashMap<>();
             for(String s : ORDER_GOODS_ONE_PAGE_ARR){
-                onePageTempStr = onePageTempStr.replace("${" + s + "}", onePageData.get(s));
+                onePageData.put(s, dataRecord.getStr(s));
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if(br != null){
-                try {
-                    br.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
+            File onePageTemp = new File(this.getRequest().getSession().getServletContext().getRealPath("") + "/template/order.template");
+            BufferedReader br = null;
+            String onePageTempStr = "";
+            try {
+                br = new BufferedReader(new InputStreamReader(new FileInputStream(onePageTemp), "utf-8"));
+                String str = br.readLine();
+                while(str != null){
+                    onePageTempStr += str;
+                    str = br.readLine();
+                }
+                for(String s : ORDER_GOODS_ONE_PAGE_ARR){
+                    onePageTempStr = onePageTempStr.replace("${" + s + "}", onePageData.get(s));
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if(br != null){
+                    try {
+                        br.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
-        }
-        PDFUtil pdfUtil = new PDFUtil(this.getRequest(), "template");
-        Map<String,Object> data = new HashMap();
-        String table = "";
-        String tableStr = "";
+
+            Map<String,Object> data = new HashMap();
+            String table = "";
+            String tableStr = "";
 //        String title = "<table class=\"order-list\"><thead style=\"display:table-header-group\"><tr><td colspan=\"5\"><table class=\"order-top\"><tr><th colspan=\"2\" align=\"center\">出库单</th></tr><tr><td width=\"60%\">出库单号：${order_num}</td><td>仓库：${warehouse_name}</td></tr><tr><td width=\"60%\">出库日期：${date}</td><td>门店：${store_name}</td></tr></table></td></tr></thead></table>";
 //        for(String s : outgoing_goods_one_page_arr){
 //            title = title.replace("${" + s + "}", onePageData.get(s));
 //        }
-        String order_detail_sql="SELECT odid AS product_id,odname AS product_name,odquantity AS product_quantity,odcurrent_price AS product_price FROM w_orderform_detail WHERE oid =?";
-        List<Record> dataList = Db.find(order_detail_sql, orderId);
-        if(dataList != null && dataList.size() > 0){
-            int i = 1;
-            for(int j = 0; j < dataList.size(); j++,i++){
-                Record r = dataList.get(j);
-                tableStr += "<tr><td>" + r.get("product_id") + "</td><td>" + r.get("product_name") + "</td><td>" + r.get("product_quantity") + "</td><td>" + r.get("product_price") + "</td></tr>";
-                //为什么要%31
-                if(i % 4 == 0){
+            String order_detail_sql="SELECT odid AS product_id,odname AS product_name,odquantity AS product_quantity,odcurrent_price AS product_price FROM w_orderform_detail WHERE oid =?";
+            List<Record> dataList = Db.find(order_detail_sql, orderId);
+            if(dataList != null && dataList.size() > 0){
+                int k = 1;
+                for(int j = 0; j < dataList.size(); j++,k++){
+                    Record r = dataList.get(j);
+                    tableStr += "<tr><td>" + r.get("product_id") + "</td><td>" + r.get("product_name") + "</td><td>" + r.get("product_quantity") + "</td><td>" + r.get("product_price") + "</td></tr>";
+                    if(k % 29 == 0){
+                        table += onePageTempStr.replace("${table}", tableStr);
+                        table += "<div class='pageNext'></div>";
+                        tableStr = "";
+                    }
+                }
+                if(tableStr != null && tableStr.length() > 0){
                     table += onePageTempStr.replace("${table}", tableStr);
-                    table += "<div class='pageNext'></div>";
-                    tableStr = "";
                 }
             }
-            if(tableStr != null && tableStr.length() > 0){
-                table += onePageTempStr.replace("${table}", tableStr);
-            }
+            data.put("table", table);
+            //data.put("creater_name", new UserSessionUtil(getRequest()).getRealName());
+            data.put("print_time", DateTool.GetDateTime());
+            data.put("product_sum", dataRecord.getStr("product_sum"));
+            data.put("freight", "100");
+            data.put("should_pay", dataRecord.getStr("should_pay"));
+            data.put("creater_name", "admin");
+            content += pdfUtil.loadDataByTemplate(data, "orderTemplate.html");
         }
-        data.put("table", table);
-        data.put("creater_name", new UserSessionUtil(getRequest()).getRealName());
-        data.put("creater_name", "uytrfds");
-        String content = pdfUtil.loadDataByTemplate(data, "orderTemplate.html");
-        try {
-            String name = UUIDTool.getUUID();
-            pdfUtil.createPdf(content, this.getRequest().getSession().getServletContext().getRealPath("") + "/pdf/" + name + ".pdf");
+            try {
+                String name = UUIDTool.getUUID();
+                pdfUtil.createPdf(content, this.getRequest().getSession().getServletContext().getRealPath("") + "/pdf/" + name + ".pdf");
 //            createPrintDetails(orderId, dataRecord, "outgoing_goods");
 //            this.getResponse().sendRedirect(getRequest().getContextPath()  + "/pdf/" + name + ".pdf");
 //            this.getRequest().getRequestDispatcher(getRequest().getContextPath()  + "/pdf/a.pdf");
-            Record result = new Record();
-            result.set("pageUrl", getRequest().getScheme()+"://"+getRequest().getServerName()+":"+getRequest() .getServerPort()  + "/pdf/" + name + ".pdf");
-            result.set("title", "客户订单");
-            jhm.put("data", result);
-        } catch (Exception e) {
-            e.printStackTrace();
-            jhm.putMessage(e.getMessage()).putCode(-1);
-        }
+                Record result = new Record();
+                result.set("pdfUrl", getRequest().getScheme()+"://"+getRequest().getServerName()+":"+getRequest() .getServerPort()  + "/pdf/" + name + ".pdf");
+                result.set("title", "客户订单");
+                jhm.put("data", result);
+            } catch (Exception e) {
+                e.printStackTrace();
+                jhm.putMessage(e.getMessage()).putCode(-1);
+            }
+
         renderJson(jhm);
     }
 
