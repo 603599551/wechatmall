def43272970b9cf04fa5a01bb5fc2f34cf7b934a
zhanglei <807071708@qq.com>
2018-10-01 10:56:43 +0800

    UserCtrl修改完成

--- src/com/wechatmall/pc/system/UserCtrl.java
+++ src/com/wechatmall/pc/system/UserCtrl.java
@@ -111,7 +111,7 @@ public class UserCtrl extends BaseCtrl{
         List<Object> params = new ArrayList<>();
         List<Object> list = new ArrayList<>();
         String select = "select *";
-        String sql = " from (select wa.username, wa.`password`, wa. name nickname,(select id from h_job where wa.job_id = h_job.id) job_id,( select name from h_job where wa.job_id = h_job.id ) job, wa.status, wa.creater_id, wa.id from w_admin wa order by wa.creater_id desc) a where 1=1";
+        String sql = " from (select wa.username, wa.`password`, wa. name nickname, (select id from h_job where wa.job_id = h_job.id) job_id,( select name from h_job where wa.job_id = h_job.id ) job, wa.status, wa.creater_id, wa.id from w_admin wa order by wa.creater_id desc) a where 1=1";
         if(name != null && name.length() > 0){
             name = "%" + name + "%";
             sql += "  and a.nickname like ? ";
