3b89655e11bb7888911186686560ea274fe95a22
chenjiarui <2601992846@qq.com>
2018-10-04 17:53:59 +0800

    商城商品公告回显

--- src/com/wechatmall/mobile/mall/MallCtrl.java
+++ src/com/wechatmall/mobile/mall/MallCtrl.java
@@ -477,8 +477,8 @@ public class MallCtrl extends BaseCtrl {
             *消息查询
             * 从w_notice表中查询ncontent和nmodify_time字段
              */
-            String sql = "select ncontent content,nmodify_time time from w_notice where ncreator_id = ? ORDER BY nmodify_time desc";
-            List<Record> showHarvestMassageList = Db.find(sql,userId);
+            String sql = "select ncontent content,nmodify_time time from w_notice ORDER BY nmodify_time desc";
+            List<Record> showHarvestMassageList = Db.find(sql);
             if(showHarvestMassageList != null && showHarvestMassageList.size()>0) {
                 jhm.put("notice",showHarvestMassageList);
             } else {
